5feac8030b7e575104da3ef39bb43fdac89f2b55
wangyongqing01 <wangyongqing01@baidu.com>
2018-09-05 18:47:25 +0800

    增加 test

--- test/test.js
+++ test/test.js
@@ -1,37 +1,34 @@
 const vfs = require('vinyl-fs');
 const path = require('path');
 const through = require('through2');
-const concat = require('concat-stream');
-
+const filter = require('gulp-filter');
+const rename = require('gulp-rename');
+const f = filter(['**/_*'], {
+    restore: true
+});
 vfs.src(['**/*', '!node_modules/**/*'], {cwd: path.join(__dirname, '../test/project'), cwdbase: true, dot: true})
-    // 2. 增加 handlerbar
-    // 处理 _ 开头文件为 .开头
-    .pipe(through.obj((file, enc, callback) => {
-        if (file.isNull()) {
-            return callback(null, file);
-        }
-        else if (file.isBuffer()) {
-        }
-        else if (file.isStream()) {
-        }
+    .pipe(f)
+    .pipe(rename(path => {
+        path.extname = path.basename.replace(/^_/, '.')
+        path.basename = '';
 
-        if (file.isBuffer()) {
-            let content = file.contents.toString(enc);
-            // console.log(content);
-            file.contents = new Buffer('修改也');
-            console.log(file.contents.toString(enc));
-        }
-
-        callback(null, file);
+        return path;
     }))
+    .pipe(f.restore)
+
     .pipe(through.obj((file, enc, callback) => {
         if (file.isBuffer()) {
             let content = file.contents.toString(enc);
             console.log(content);
         }
+
         callback(null, file);
     }))
+    .pipe(vfs.dest('./dist'))
+
     .on('end', () => {
         console.log(1);
-    })
+    }).on('error', err => {
+    console.log(err);
+})
     .resume();
