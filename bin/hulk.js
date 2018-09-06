47c4f8f1d041b0ea9d764146d4eebbded43d8ee0
wangyongqing01 <wangyongqing01@baidu.com>
2018-09-06 15:39:09 +0800

    完成 init 功能

--- bin/hulk.js
+++ bin/hulk.js
@@ -7,11 +7,73 @@ const requiredVersion = require('../package.json').engines.node;
 function checkNodeVersion(wanted, id) {
     if (!semver.satisfies(process.version, wanted)) {
         console.log(chalk.red(
-            'You are using Node ' + process.version + ', but this version of ' + id +
-            ' requires Node ' + wanted + '.\nPlease upgrade your Node version.'
+            '当前你的 Node 版本为 ' + process.version + '，运行 ' + id
+            + ' 需要 Node ' + wanted + ' 以上版本。\n请升级你的 Node'
         ));
         process.exit(1);
     }
 }
 checkNodeVersion(requiredVersion, 'hulk-cli');
+const program = require('commander');
+const loadCommand = require('../lib/load-command');
 
+program
+    .version(require('../package').version, '-v --version')
+    .usage('<command> [options]');
+
+program
+    .command('init <template> <appName>')
+    .description('通过模板初始化项目')
+    .option('-o, --offline', '这是一个本地路径')
+    .option('-h, --https', 'Git 使用 https 请求')
+    .option('-u, --user <user>', 'Git 用户名，默认：git')
+    .option('-I, --no-install', '不安装依赖，默认：安装')
+    .option('-f, --force', 'force', '强制删除已存在的目录，默认：删除')
+    .option('-r, --registry <registry>', '设置 npm registry')
+    .option('-c, --cache', '优先使用缓存')
+    .action((template, appName, cmd) => {
+        loadCommand('init', {
+            template,
+            appName
+        }, cmd);
+    });
+
+program
+    .command('init-template <appName>')
+    .description('生成一个项目模板')
+    .action(() => {
+        loadCommand('init-template', {
+            appName
+        }, cmd);
+    });
+
+program
+    .command('add <type> <name>')
+    .description('添加组件、插件等')
+    .action((type, name, cmd) => {
+        loadCommand('add', {
+            type,
+            name
+        }, cmd);
+    });
+
+program
+    .arguments('<command>')
+    .action((cmd) => {
+        program.outputHelp();
+        console.log(`  ` + chalk.red(`找不到命令 ${chalk.yellow(cmd)}.`));
+        console.log();
+    });
+
+program.on('--help', () => {
+    console.log();
+    console.log(`  使用 ${chalk.cyan(`hulk <command> --help`)} 查看帮助`);
+    console.log();
+});
+program.commands.forEach(c => c.on('--help', () => console.log()));
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
---
+++ command/init.js
@@ -0,0 +1,124 @@
+/**
+ * @file init 初始化项目
+ */
+const path = require('path');
+const fs = require('fs-extra');
+
+const exists = fs.existsSync;
+
+const home = require('user-home');
+const inquirer = require('inquirer');
+const rm = fs.removeSync;
+// const debug = require('debug')('init');
+
+const download = require('../lib/repo-download');
+const generate = require('../lib/generate');
+
+const {
+    chalk,
+    isLocalPath,
+    getTemplatePath,
+    error,
+    logWithSpinner,
+    stopSpinner,
+    log,
+    clearConsole
+} = require('../lib/utils');
+
+module.exports = async (context, argv, opts) => {
+    const template = argv.template;
+    const appName = argv.appName;
+    const inPlace = !appName || appName === '.';
+    const name = inPlace ? path.relative('../', process.cwd()) : appName;
+    const dest = path.resolve(appName || '.');
+    // 临时存放地址，存放在~/.hulk-templates 下面
+    let tmp = path.join(home, '.hulk-templates', template.replace(/[\/:#]/g, '-'));
+
+    if (exists(dest)) {
+        if (opts.force) {
+            await fs.remove(dest);
+        }
+        else {
+            clearConsole();
+            if (inPlace) {
+                const {ok} = await inquirer.prompt([
+                    {
+                        name: 'ok',
+                        type: 'confirm',
+                        message: '在当前目录创建项目？'
+                    }
+                ]);
+                if (!ok) {
+                    return;
+                }
+            }
+            else {
+                const {action} = await inquirer.prompt([
+                    {
+                        name: 'action',
+                        type: 'list',
+                        message: `目录 ${chalk.cyan(dest)} 已经存在。请选择操作：`,
+                        choices: [
+                            {name: '覆盖', value: 'overwrite'},
+                            {name: '合并', value: 'merge'},
+                            {name: '取消', value: false}
+                        ]
+                    }
+                ]);
+                if (!action) {
+                    return;
+                }
+                else if (action === 'overwrite') {
+                    log(`删除 ${chalk.cyan(targetDir)}...`);
+                    await fs.remove(targetDir);
+                }
+            }
+        }
+    }
+
+    const isOffline = opts.offline;
+    if (isOffline || isLocalPath(template)) {
+        // 使用离线地址
+        // 直接复制，不下载 icode 代码
+        const templatePath = getTemplatePath(template);
+        if (exists(templatePath)) {
+            generate(name, templatePath, dest, opts);
+        }
+        else {
+            error('模板文件不存在');
+        }
+    }
+    else {
+        if (!~template.indexOf('/')) {
+            tmp = path.join(home, '.hulk-templates', template.replace(/[\/:#]/g, '-'));
+        }
+
+        if (opts.cache && exists(tmp)) {
+            // 优先使用缓存
+            generate(name, tmp, dest, opts);
+        }
+        else {
+
+            clearConsole();
+            logWithSpinner('🗃', '下载模板...');
+            if (exists(tmp)) {
+                rm(tmp);
+            }
+
+            download(template, tmp, opts, err => {
+                stopSpinner();
+                if (!err) {
+                    clearConsole();
+                    generate(name, tmp, dest, opts);
+                }
+                else {
+                    error('拉取代码失败，请检查路径和代码权限是否正确');
+                    if (!process.env.DEBUG) {
+                        log(`使用「${chalk.bgYellow.black('DEBUG=*')}」 ，查看报错信息`);
+                    }
+                }
+            });
+        }
+    }
+
+};
