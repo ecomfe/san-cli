d5f25b710b1a2eae711f5df69633a395ca21a2ca
wangyongqing01 <wangyongqing01@baidu.com>
2018-11-21 13:17:47 +0800

    增加 serve

--- command/serve.js
+++ command/serve.js
@@ -3,5 +3,13 @@
  * @file Created on Tue Nov 20 2018
  * @author wangyongqing <wangyongqing01@baidu.com>
  */
-
-module.exports = (argv, opts) => {};
+const path = require('path');
+const {existsSync} = require('fs-extra');
+const {error} = require('../lib/utils');
+module.exports = ({filename}, opts) => {
+    const filepath = path.resolve(filename);
+    if (!existsSync(filepath)) {
+        return error(`${filename} is not exist`);
+    }
+    
+};
