3dd556e18387d5fb4eccc21648cfd85adc30901d
wangyongqing01 <wangyongqing01@baidu.com>
2019-02-19 15:29:35 +0800

    修复bugbye

--- packages/@baidu/hulk-command-component/loaders/utils/compiler.js
+++ packages/@baidu/hulk-command-component/loaders/utils/compiler.js
@@ -2,53 +2,43 @@
  * @file 解析器
  * @author wangyongqing <wangyongqing01@baidu.com>
  */
-const prism = require('prismjs');
-const marked = require('marked');
-
-const {
-    slugify,
-    helper,
-    merge
-} = require('./index.js');
-
-const renderer = new marked.Renderer();
-
-const origin = {};
-
 /**
  * 来自 docsify
  * Render anchor tag
  * @link https://github.com/markedjs/marked#overriding-renderer-methods
  */
-origin.heading = renderer.heading = function (text, level) {
-    var slug = slugify(text);
-    var url = '#' + slug;
+const prism = require('prismjs');
+const marked = require('marked');
 
-    return ('<h' + level + ' id="' + slug + '"><span>' + text + '</span><a href="' + url + '" class="anchor">#</a></h' + level + '>');
+const {slugify, helper, merge} = require('./index.js');
+
+const renderer = new marked.Renderer();
+const origin = {};
+
+origin.heading = renderer.heading = (text, level) => {
+    const slug = slugify(text);
+    const url = '#' + slug;
+
+    return `<h${level} id="${slug}"><span>${text}</span><a href="${url}" class="anchor">#<a></h${level}>`;
 };
 // Highlight code
-origin.code = renderer.code = function (code, lang) {
-    if (lang === void 0) {
+origin.code = renderer.code = (code, lang = '') => {
+    if (lang === undefined) {
         lang = '';
     }
 
-    var hl = prism.highlight(
-        code,
-        prism.languages[lang] || prism.languages.markup
-    );
+    const hl = prism.highlight(code, prism.languages[lang] || prism.languages.markup);
 
-    return ('<pre v-pre data-lang="' + lang + '"><code class="lang-' + lang + '">' + hl + '</code></pre>');
+    return `<pre v-pre data-lang="' + lang + '"><code class="lang-' + lang + '">${hl}</code></pre>`;
 };
 
-origin.paragraph = renderer.paragraph = function (text) {
-    var result;
+origin.paragraph = renderer.paragraph = text => {
+    let result;
     if (/^!&gt;/.test(text)) {
         result = helper('tip', text);
-    }
-    else if (/^\?&gt;/.test(text)) {
+    } else if (/^\?&gt;/.test(text)) {
         result = helper('warn', text);
-    }
-    else {
+    } else {
         result = '<p>' + text + '</p>';
     }
     return result;
@@ -60,8 +50,7 @@ function isPrimitive(value) {
     return typeof value === 'string' || typeof value === 'number';
 }
 
-module.exports = function (text, options) {
-    options = options || {};
+module.exports = (text, options = {}) => {
     marked.setOptions(
         merge(options, {
             renderer: merge(renderer, options.renderer || {})
@@ -71,8 +60,7 @@ module.exports = function (text, options) {
     let html;
     if (isPrimitive(text)) {
         html = marked(text);
-    }
-    else {
+    } else {
         html = marked.parser(text);
     }
 
