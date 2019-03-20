01519067213c72de44eca0a6ab9a26c325fb4673
wangyongqing01 <wangyongqing01@baidu.com>
2019-03-20 09:48:34 +0800

    add hulk-cli/template file

---
+++ packages/hulk-cli/template/main.js
@@ -0,0 +1,11 @@
+/**
+ * 页面的 main.js
+ * @file Created on Thu Nov 15 2018
+ * @author wangyongqing <wangyongqing01@baidu.com>
+ */
+
+import {defineComponent} from 'san';
+import App from '~entry';
+const AppC = defineComponent(App);
+const app = new AppC();
+app.attach(document.getElementById('app'));
