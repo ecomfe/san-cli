9ac51f477d0199ebc30f834c908aa9563bf41077
wangyongqing01 <wangyongqing01@baidu.com>
2019-05-09 10:59:32 +0800

    fix: component 统一使用 constanst 文件

--- packages/hulk-cli/commands/component/index.js
+++ packages/hulk-cli/commands/component/index.js
@@ -3,14 +3,18 @@
  * @author wangyongqing <wangyongqing01@baidu.com>
  */
 // alias serve + change (entry + app)
+const {ENV, DEVELOPMENT_MODE} = require('../../constants');
+
 module.exports = program => {
+    const envReg = new RegExp('^(' + ENV.join('|') + ')$', 'i');
+
     program
         .command('component <entry>')
         .alias('md')
         .description('San 组件 Demo 预览服务器')
-        .option('-p, --port <port>', 'dev server port', /\d+/, 8899)
+        .option('-p, --port <port>', 'dev server port', /\d+/)
         .option('-h, --host <host>', 'dev server host')
-        .option('-m, --mode <mode>', '设置 webpack mode', /^(development|production)$/i, 'development')
+        .option('-m, --mode <mode>', '指定 webpack mode', envReg, DEVELOPMENT_MODE)
         .option('-c, --config <config>', '设置  webpack config 文件')
         .option('--use-https', 'use https')
         .action((entry, args) => {
