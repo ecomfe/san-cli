62741dcecdb0746f814ac0c39577637d06bf0857
wangyongqing01 <wangyongqing01@baidu.com>
2019-05-15 15:53:31 +0800

    feat: 1. 增加 build 几个option；2. --analyze 重新修改

--- packages/hulk-cli/commands/build/index.js
+++ packages/hulk-cli/commands/build/index.js
@@ -8,11 +8,23 @@ module.exports = program => {
         .command('build [entry]')
         .description('内置 Webpack 零配置打包')
         .option('-d, --dest <dir>', '输出文件路径')
-        .option('--analyze', '分析 bunlde')
-        .option('--modern', 'modern mode')
+        .option('--analyze', 'webpack-analyze-bunlde 模式')
+        .option('--modern', 'modern 模式打包')
         .option('--watch', 'watch 模式')
         .option('-m, --mode <mode>', '设置 webpack mode', /^(development|production)$/i, 'production')
         .option('-c, --config <config>', '指定 webpack config 地址')
         .option('--no-clean', '构建之前不删除 dist 目录')
+        .option('--no-colors, --no-color', 'log 不显示颜色')
+        // Accepted values: none, errors-only, minimal, normal, detailed, verbose
+        // Any other falsy value will behave as 'none', truthy values as 'normal'
+        .option(
+            '--stats <stats>',
+            '显示webpack stats 参数',
+            /^(none|table|errors-only|minimal|normal|table|detailed)$/i,
+            'table'
+        )
+        .option('--verbose', 'verbose log')
+        .option('--report-json', '生成打包报告 report.json')
+        .option('--report', '生成打包报告 report.html')
         .action(run);
 };
--- packages/hulk-cli/commands/build/run.js
+++ packages/hulk-cli/commands/build/run.js
@@ -15,21 +15,25 @@ const modifyConfig = (config, fn) => {
         fn(config);
     }
 };
-
-module.exports = async (initEntry, args) => {
+/* eslint-disable fecs-valid-jsdoc */
+/**
+ * 打包流程，这里可以单独给其他模块使用
+ * initEntry，是 hulk build [entry]传入的 entry，默认不传则使用 hulk.config.js 配置 pages 打包
+ * args 是 hulk Build 的 optional arguments
+ * plugins 是专门给 import 模式使用的参数，传入 Service plugin，参考 lib/serivce-plugins
+ */
+module.exports = async (initEntry, args, plugins = []) => {
     // 开始时间
     const startTime = Date.now();
     const chalk = require('@baidu/hulk-utils/chalk');
-    const report = require('../../lib/report');
 
-    // 处理 entry 不存在的情况
+    // 1. 处理 entry 不存在的情况， entry 是 hulk build [entry]传入的optional arguments
     const resolveEntry = require('../../lib/utils').resolveEntry;
     args.entry = initEntry;
     if (initEntry) {
         const {entry, isFile} = resolveEntry(initEntry);
         args.entry = entry;
-        args.target = args.target ? args.target : isFile ? 'page' : 'app';
-        args._isFile = isFile;
+        args.target = isFile ? 'entry' : 'app';
     } else {
         args.target = 'app';
     }
@@ -37,9 +41,18 @@ module.exports = async (initEntry, args) => {
     const mode = args.mode || process.env.NODE_ENV || 'production'; // 默认是 production
     args.mode = mode;
 
+    if (args.modern && args.analyze) {
+        // --modern + --analyze 应该显示 modern 的 analyze 的结果
+        process.env.HULK_CLI_MODERN_BUILD = true;
+    }
+
     const bundleTag = args.modern ? (process.env.HULK_CLI_MODERN_BUILD ? 'modern bundle ' : 'legacy bundle ') : '';
     info(`Building ${bundleTag}for ${mode}...`);
 
+    /**
+     * 失败处理逻辑
+     * @param {Error|Stats} err - error 对象
+     */
     function failBuild(err) {
         if (err && err.toJson) {
             console.log('Build failed with errors.');
@@ -49,63 +62,78 @@ module.exports = async (initEntry, args) => {
             console.log(err);
         }
     }
-    function successBuild({stats, config, options}, {isModern, isModernBuild} = {}) {
-        const targetDir = path.resolve(context, config.output.path || args.dest || options.outputDir);
-        const targetDirShort = path.relative(context, targetDir);
-
-        stats = stats.toJson({
-            all: false,
-            entrypoints: true,
-            assets: true,
-            chunks: true,
-            version: true,
-            timings: true,
-            performance: true
-        });
-
+    // 编译成功处理逻辑
+    function successBuild({stats: webpackStats, config, options}, {isModern, isModernBuild} = {}) {
         if (!args.analyze) {
-            console.log(
-                report(stats, targetDirShort, {
-                    resolve: p => path.resolve(context, p)
-                })
-            );
-        }
-        if (!args.watch) {
-            const duration = (Date.now() - startTime) / 1e3;
-            if (isModern) {
-                if (isModernBuild) {
-                    success('Build modern bundle success');
-                } else {
-                    success('Build legacy bundle success');
-                    console.log();
-                }
-                return;
+            // 只有在非 analyze 模式下才会输出 log
+            const targetDir = path.resolve(context, config.output.path || args.dest || options.outputDir);
+            const targetDirShort = path.relative(context, targetDir);
+            const stats = webpackStats.toJson({
+                all: false,
+                entrypoints: true,
+                assets: true,
+                chunks: true,
+                version: true,
+                timings: true,
+                performance: true
+            });
+
+            if (args.verbose) {
+                args.stats = 'verbose';
             }
-            const {time, version} = stats;
-            // const targetDirShort = path.relative(context, targetDir);
-            success(
-                `The ${chalk.cyan(targetDirShort)} directory is ready to be deployed. Duration ${chalk.cyan(
-                    `${duration}/${time / 1e3}s`
-                )}, Webpack ${version}.`
-            );
-            // 解决 apim 这类问题
-            // process.exit(0);
-        } else {
+            if (args.stats === 'table') {
+                console.log(
+                    require('../../lib/report')(stats, targetDirShort, {
+                        resolve: p => path.resolve(context, p)
+                    })
+                );
+            } else {
+                const Stats = require('webpack/lib/Stats');
+                const statsOptions = Stats.presetToOptions(args.stats);
+                statsOptions.colors = !!args.colors || !!args.color;
+                console.log(webpackStats.toString(statsOptions));
+            }
+            if (!args.watch) {
+                const duration = (Date.now() - startTime) / 1e3;
+                if (isModern) {
+                    if (isModernBuild) {
+                        success('Build modern bundle success');
+                    } else {
+                        success('Build legacy bundle success');
+                        console.log();
+                    }
+                    return;
+                }
+                const {time, version} = stats;
+                success(
+                    `The ${chalk.cyan(targetDirShort)} directory is ready to be deployed. Duration ${chalk.cyan(
+                        `${duration}/${time / 1e3}s`
+                    )}, Webpack ${version}.`
+                );
+            }
+        }
+        if (args.watch) {
             success('Build complete. Watching for changes...');
         }
     }
+
+    // 2. 调用 build 函数开始正式的编译
     if (args.modern) {
+        // 2.1 modern mode，会fork execa 执行一次打包
+        // modern mode 必须要保证 legacy 先打包完成
         if (!process.env.HULK_CLI_MODERN_BUILD) {
-            // main-process for legacy build
+            // for legacy build
+            // 要保证打包顺序，所以这里用 await！
             const data = await build(
                 Object.assign({}, args, {
                     modernBuild: false
-                })
+                }),
+                plugins
             );
             if (data) {
                 successBuild(data, {isModern: true});
             }
-            // spawn sub-process of self for modern build
+            //  execa 打包，保证打包环境的纯洁性
             const execa = require('execa');
             const cliBin = require('path').resolve(__dirname, '../../bin/hulk.js');
             const rawArgs = process.argv.slice(3);
@@ -117,41 +145,39 @@ module.exports = async (initEntry, args) => {
                 }
             });
         } else {
-            // sub-process for modern build
+            // 这里是 modern mode 的打包
+            // 注意要用 clean = false 哦！！！不然会删掉 legacy-${filename}.json，legacy 打包就白费了！
             build(
                 Object.assign({}, args, {
                     modernBuild: true,
                     clean: false
-                })
+                }),
+                plugins
             )
                 .then(data => successBuild(data, {isModern: true, isModernBuild: true}))
                 .catch(err => failBuild(err));
         }
     } else {
-        build(args)
+        // 普通模式打包
+        build(args, plugins)
             .then(data => successBuild(data))
             .catch(err => failBuild(err));
     }
 };
 
-async function build(args) {
+async function build(args, plugins = []) {
     const {mode, target, config: configFile, entry, modern = false, dest, watch} = args;
 
     const fse = require('fs-extra');
     const webpack = require('webpack');
     const Service = require('../../lib/Service');
 
-    const plugins = [];
-    if (args.analyze) {
-        // 添加 analyze
-        plugins.push(require('../../lib/serivce-plugins/analyze'));
-    }
-
     const service = new Service(context, {
         configFile,
         plugins
     });
 
+    // 这里的 options 是结合 hulk.configl.js 处理之后的 options
     const options = service.init(mode, {
         target,
         modernMode: modern,
@@ -166,15 +192,14 @@ async function build(args) {
     }
 
     const chainConfig = service.resolveChainableWebpackConfig();
-    if (args.modern) {
+    if (args.modern && !args.analyze) {
         const ModernModePlugin = require('../../lib/webpack/ModernModePlugin');
         if (!args.modernBuild) {
             // Inject plugin to extract build stats and write to disk
             chainConfig.plugin('modern-mode-legacy').use(ModernModePlugin, [
                 {
                     targetDir,
-                    isModernBuild: false,
-                    unsafeInline: args['unsafe-inline']
+                    isModernBuild: false
                 }
             ]);
         } else {
@@ -188,6 +213,27 @@ async function build(args) {
         }
     }
 
+    if (args.analyze) {
+        // 添加 analyze
+        const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
+        chainConfig.plugin('bundle-analyzer').use(new BundleAnalyzerPlugin());
+    } else if (args.report || args['report-json']) {
+        const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+        // 单独标示 modern 打包
+        const bundleName = args.modern ? (args.modernBuild ? 'modern-' : 'legacy-') : '';
+        chainConfig.plugin('bundle-analyzer').use(
+            new BundleAnalyzerPlugin({
+                logLevel: 'warn',
+                openAnalyzer: false,
+                analyzerMode: args.report ? 'static' : 'disabled',
+                reportFilename: `${bundleName}report.html`,
+                statsFilename: `${bundleName}report.json`,
+                generateStatsFile: !!args['report-json']
+            })
+        );
+    }
+
     // resolve webpack config
     // const webpackConfig = service.resolveWebpackConfig();
     const webpackConfig = service.resolveWebpackConfig(chainConfig);
@@ -199,7 +245,7 @@ async function build(args) {
         });
     }
 
-    // watch 功能
+    // --watch 功能
     if (watch) {
         modifyConfig(webpackConfig, config => {
             config.watch = true;
@@ -208,7 +254,7 @@ async function build(args) {
 
     webpackConfig.mode = mode;
 
-    if (args._isFile && entry) {
+    if (target === 'entry' && entry) {
         if (/\.san$/.test(entry)) {
             webpackConfig.resolve.alias['~entry'] = path.resolve(context, entry);
         } else {
@@ -216,12 +262,10 @@ async function build(args) {
                 app: entry
             };
         }
-        // } else {
         // 默认hulk.config page 里面配置的就有 app 呢？
         // delete webpackConfig.entry.app;
     } else if (entry && !options.pages) {
         webpackConfig.entry = {app: path.resolve(context, entry)};
-        // } else {
         // 默认hulk.config page 里面配置的就有 app 呢？
         // delete webpackConfig.entry.app;
     }
@@ -244,7 +288,7 @@ async function build(args) {
             if (stats.hasErrors()) {
                 return reject(stats);
             }
-
+            // 这里 resolve 传出三个值：stats config 和 options
             resolve({stats, config: webpackConfig, options});
         });
     });
