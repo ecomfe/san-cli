d5c013c47b3b5129fede6b47ab7e8f65071c9b31
wangyongqing01 <wangyongqing01@baidu.com>
2019-07-18 14:20:30 +0800

    feat: 自动部署脚本

---
+++ doc-site.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# 保证build.sh脚本有任何错误就退出
+set -e
+export PATH=$NODEJS_BIN_LATEST:$PATH
+
+# echo "node: $(node -v)"
+# echo "npm: v$(npm -v)"
+
+moduleName=hulk-docs
+
+tar czf ${moduleName}.tar.gz ./docs
