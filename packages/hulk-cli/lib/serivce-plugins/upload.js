5302884ce0caeed5227f96d8ba1723acfcb8c8ae
jinzhan <jinzhan@baidu.com>
2019-08-30 18:02:00 +0800

    feat: 增加fsr上传功能

--- packages/hulk-cli/lib/serivce-plugins/upload.js
+++ packages/hulk-cli/lib/serivce-plugins/upload.js
@@ -4,28 +4,31 @@
  */
 
 const upload = require('@baidu/upload-file');
+const fsrUpload = require('@baidu/upload-file/fsr');
 const {success, error, info} = require('@baidu/hulk-utils/logger');
 
 /*
-* Hulk.config.js中配置deployMap字段
-*
-* 配置示例:
-*
-* ...
-*
-* deployMap: {
-*    sandbox: {
-*        receiver: 'http://fe.fis.searchbox.otp.baidu.com/fis/receiver',
-*        templatePath: '/home/work/nginx_static/html/test/template',
-*        staticPath: '//home/work/nginx_static/html/test/static',
-*        staticDomain: 'http://test.baidu.com:8888'
-*    }
-* }
-*
-* build命令：hulk build --config hulk.config.js -r sandbox
-* ...
-*
-* **/
+ * Hulk.config.js中配置deployMap字段
+ *
+ * 配置示例:
+ *
+ * ...
+ *
+ * deployMap: {
+ *    sandbox: {
+ *        receiver: 'http://fe.fis.searchbox.otp.baidu.com/fis/receiver',
+ *        templatePath: '/home/work/nginx_static/html/test/template',
+ *        staticPath: '//home/work/nginx_static/html/test/static',
+ *        staticDomain: 'http://test.baidu.com:8888'
+ *    }
+ * }
+ *
+ * build命令：hulk build --config hulk.config.js -r sandbox
+ * ...
+ *
+ * 使用FIS安全部署服务
+ * http://agroup.baidu.com/fis/md/article/196978?side=folder
+ * **/
 
 const PLUGIN_NAME = 'HulkUploadPlugin';
 
@@ -37,14 +40,24 @@ class Upload {
     apply(compiler) {
         const options = this.options;
         compiler.hooks.emit.tapAsync(PLUGIN_NAME, (compilation, callback) => {
-            Object.keys(compilation.assets).forEach(filename => {
+            const targetFiles = Object.keys(compilation.assets).map(filename => {
                 const to = /\.tpl$/.test(filename) ? options.templatePath : options.staticPath;
-                upload({
+                return {
+                    host: options.host,
                     receiver: options.receiver,
-                    to,
                     content: this.getContent(filename, compilation),
-                    filePath: filename
-                });
+                    to,
+                    subpath: filename
+                };
+            });
+            // FIS安全部署服务：http://agroup.baidu.com/fis/md/article/196978?side=folder
+            const uploadHandler = options.disableFsr ? upload : fsrUpload;
+            uploadHandler(targetFiles, {
+                host: options.host,
+                retry: 2
+            }, () => {
+                console.log('\n');
+                console.log('UPLOAD COMPLETED!');
             });
             callback();
         });
