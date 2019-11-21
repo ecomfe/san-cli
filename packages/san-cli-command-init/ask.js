438112f99f00224c713f4be304a7344e5b303cbf
yanyiting <yanyiting@baidu.com>
2019-11-21 17:44:34 +0800

    feat: 新增init单测

--- packages/san-cli-command-init/ask.js
+++ packages/san-cli-command-init/ask.js
@@ -12,17 +12,14 @@ const promptMapping = {
     boolean: 'confirm'
 };
 
-module.exports = (prompts, data) => {
+module.exports = async (prompts, data) => {
     const answers = {};
     let keys = Object.keys(prompts);
-
-    return new Promise(async (resolve, reject) => {
-        let key;
-        while ((key = keys.shift())) {
-            await prompt(answers, key, prompts[key], data);
-        }
-        resolve(answers);
-    });
+    let key;
+    while ((key = keys.shift())) {
+        await prompt(answers, key, prompts[key], data);
+    }
+    return Promise.resolve(answers);
 };
 // 将 default 使用 templateData 渲染一下，比如作者之类的
 // default中有{{author}}类似这样的，渲染一下，填上
@@ -38,45 +35,43 @@ function render(content, data) {
 
     return content;
 }
-function prompt(data, key, prompt, tplData) {
-    return new Promise(async (resolve, reject) => {
-        // 当 when 起作用的时候跳过
-        if (prompt.when && !evaluate(prompt.when, data)) {
-            resolve();
-            return;
-        }
+async function prompt(data, key, prompt, tplData) {
+    // 当 when 起作用的时候跳过
+    if (prompt.when && !evaluate(prompt.when, data)) {
+        // resolve();
+        return;
+    }
 
-        let promptDefault = prompt.default;
-        if (typeof promptDefault === 'function') {
-            // eslint-disable-next-line
-            promptDefault = function() {
-                return prompt.default.bind(this)(data);
-            };
-        }
+    let promptDefault = prompt.default;
+    if (typeof promptDefault === 'function') {
+        // eslint-disable-next-line
+        promptDefault = function() {
+            return prompt.default.bind(this)(data);
+        };
+    }
 
-        const answers = await inquirer.prompt([
-            {
-                type: promptMapping[prompt.type] || prompt.type,
-                name: key,
-                message: prompt.message || prompt.label || key,
-                default: render(promptDefault, tplData),
-                choices: prompt.choices || [],
-                validate: prompt.validate || (() => true)
-            }
-        ]);
-        // 如果回答是个数组，那么就是多选答案，给增加答案为key，相应结果为true
-        if (Array.isArray(answers[key])) {
-            data[key] = {};
-            answers[key].forEach(multiChoiceAnswer => {
-                data[key][multiChoiceAnswer] = true;
-            });
-        // 如果答案是串型，转义一下双引号
-        } else if (typeof answers[key] === 'string') {
-            data[key] = answers[key].replace(/"/g, '\\"');
-        // 其他类型的直接赋值
-        } else {
-            data[key] = answers[key];
+    const answers = await inquirer.prompt([
+        {
+            type: promptMapping[prompt.type] || prompt.type,
+            name: key,
+            message: prompt.message || prompt.label || key,
+            default: render(promptDefault, tplData),
+            choices: prompt.choices || [],
+            validate: prompt.validate || (() => true)
         }
-        resolve(data);
-    });
+    ]);
+
+    // 如果回答是个数组，那么就是多选答案，给增加答案为key，相应结果为true
+    if (Array.isArray(answers[key])) {
+        data[key] = {};
+        answers[key].forEach(multiChoiceAnswer => {
+            data[key][multiChoiceAnswer] = true;
+        });
+    // 如果答案是串型，转义一下双引号
+    } else if (typeof answers[key] === 'string') {
+        data[key] = answers[key].replace(/"/g, '\\"');
+    // 其他类型的直接赋值
+    } else {
+        data[key] = answers[key];
+    }
 }
