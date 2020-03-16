45807209ed6521a2cfa1e51c3cabbaa9e80225ee
wangyongqing01 <wangyongqing01@baidu.com>
2020-03-16 11:58:17 +0800

    :ok_hand: code review

--- packages/san-cli/commands/serve/getNormalizeWebpackConfig.js
+++ packages/san-cli/commands/serve/getNormalizeWebpackConfig.js
@@ -16,7 +16,12 @@ module.exports = function getNormalizeWebpackConfig(api, projectOptions, argv) {
         }
     });
 
-    webpackConfig = resolveEntry(entry, api.resolve(entry), webpackConfig, require.resolve('@baidu/san-cli-service/template/main.js'));
+    webpackConfig = resolveEntry(
+        entry,
+        api.resolve(entry),
+        webpackConfig,
+        require.resolve('@baidu/san-cli-service/template/main.js')
+    );
     webpackConfig.devServer = Object.assign(
         {
             hot: !isProd,
