68bc791d6159d822870e35a0408c300e57f18f6c
ksky521 <ksky521@gmail.com>
2020-04-24 16:55:58 +0800

    :pencil: docit 生成doc文件

--- packages/san-cli-utils/utils.js
+++ packages/san-cli-utils/utils.js
@@ -7,5 +7,19 @@
  * @file 工具函数
  * @author wangyongqing <wangyongqing01@baidu.com>
  */
-
+const path = require('path');
+const fs = require('fs');
 exports.flatten = arr => (arr || []).reduce((prev, curr) => prev.concat(curr), []);
+exports.isDirectoryAndNotCwd = p => {
+    if (p && typeof p === 'string') {
+        const abs = path.resolve(p);
+        const cwd = process.cwd();
+        if (cwd !== abs) {
+            const stat = fs.statSync(abs);
+            if (stat.isDirectory()) {
+                return abs;
+            }
+        }
+    }
+    return false;
+};
