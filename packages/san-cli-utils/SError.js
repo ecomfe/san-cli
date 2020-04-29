3df69bc9c1787f803038bb7ebb9f04b6dfe1019c
ksky521 <ksky521@gmail.com>
2020-04-29 13:58:33 +0800

    :sparkles: SanFriendlyErrorsPlugin

--- packages/san-cli-utils/SError.js
+++ packages/san-cli-utils/SError.js
@@ -9,9 +9,15 @@
  */
 
 module.exports = class SError extends Error {
-    constructor(msg) {
+    constructor(msg, type, stack) {
         super(msg || 'San error');
-        this.name = 'SError';
-        Error.captureStackTrace(this, SError);
+        this.name = type || 'SError';
+        if (stack && typeof stack === 'string' || Array.isArray(stack)) {
+            this.stack = Array.isArray(stack) ? stack.join('\n') : stack;
+        }
+        else {
+            Error.captureStackTrace(this, SError);
+        }
+
     }
 };
