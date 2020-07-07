2f138c0c6051501a97895e002c9226a2c4d89bf6
jinzhan <jinzhan@baidu.com>
2020-07-07 23:41:05 +0800

    :lipstick: diminish css of dependcy comps

--- packages/san-cli-ui/client/components/project-dependency/dependency-item.js
+++ packages/san-cli-ui/client/components/project-dependency/dependency-item.js
@@ -1,5 +1,5 @@
 /**
- * @file 已安装依赖列表
+ * @file 项目依赖列表
  * @author sunxiaoyu333
  */
 
@@ -8,22 +8,24 @@ import {Button, Icon} from 'santd';
 import DEPENDENCYITEM from '@graphql/dependency/dependencyItem.gql';
 import DEPENDENCY_UNINSTALL from '@/graphql/dependency/dependency-uninstall.gql';
 import avatars from '@lib/utils/avatars';
-import './index.less';
+import './dependency-item.less';
 
 export default class DependenceItem extends Component {
     static template = /* html */`
-        <div class="dependency-item">
-            <div class="item">
-                <div class="image-wrap"><img class="image" src="{{avatars(item.id)}}"/></div>
-                <div class="name-wrap">
-                    <div class="name">{{item.id}}</div>
-                    <div class="version">{{$t('dependency.version')}}{{dependencyItem.current}}</div>
+        <div class="x-dependency-item">
+            <div class="pkg-icon"><img src="{{avatars(item.id)}}"/></div>
+            <div class="pkg-info">
+                <div class="pkg-name">{{item.id}}</div>
+                <div class="pkg-detail" s-if="{{dependencyItem.current}}">
+                    <span class="pkg-version">{{$t('dependency.version')}}{{dependencyItem.current}}</span>
+                    <span class="pkg-version">{{$t('dependency.lowVersion')}}{{dependencyItem.wanted}}</span>
+                    <span class="pkg-version">{{$t('dependency.currentVersion')}}{{dependencyItem.latest}}</span>
+                    <s-icon class="pkg-check-ico" type="check-circle" />
+                    <span class="status">{{$t('dependency.installed')}}</span>
+                    <span class="pkg-detail-item pkg-detail-link" on-click="onCheck">
+                        {{$t('dependency.checkDetail')}}
+                    </span>
                 </div>
-                <div class="version name-wrap">{{$t('dependency.lowVersion')}}{{dependencyItem.wanted}}</div>
-                <div class="version name-wrap">{{$t('dependency.currentVersion')}}{{dependencyItem.latest}}</div>
-                <s-icon type="check-circle" class="name-wrap"/>
-                <div class="status">{{$t('dependency.installed')}}</div>
-                <div class="check" on-click="onCheck">{{$t('dependency.checkDetail')}}</div>
             </div>
             <s-icon type="delete" on-click="onDelete" class="delete"/>
         </div>
@@ -58,8 +60,8 @@ export default class DependenceItem extends Component {
         window.open(website);
     }
 
+    // 卸载npm包
     async onDelete() {
-        // 卸载npm包
         let {id, type} = this.data.get('item');
         await this.$apollo.mutate({
             mutation: DEPENDENCY_UNINSTALL,
