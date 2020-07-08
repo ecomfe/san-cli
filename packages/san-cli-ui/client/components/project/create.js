f3e4881fdbe6db88bb998e2a4da243f2addb433e
jinzhan <jinzhan@baidu.com>
2020-07-08 14:10:07 +0800

    :boom: rename files

--- packages/san-cli-ui/client/components/project/create.js
+++ packages/san-cli-ui/client/components/project/create.js
@@ -12,7 +12,7 @@ import CONSOLE_LOG_ADDED from '@graphql/console/consoleLogAdded.gql';
 import 'santd/es/input/style';
 import 'santd/es/button/style';
 import 'santd/es/spin/style';
-import './project-create.less';
+import './create.less';
 
 export default class ProjectCreate extends createApolloComponent(Component) {
     static template = /* html */`
