d2b6f738c967c2f6daed3d133855567904a16456
zttonly <zttonly@gmail.com>
2020-07-21 21:05:55 +0800

    :lipstick: adjust welcome component style

--- packages/san-cli-ui-addon-widgets/src/components/welcome/index.js
+++ packages/san-cli-ui-addon-widgets/src/components/welcome/index.js
@@ -4,21 +4,61 @@
  */
 
 import {Component} from 'san';
+import {Icon, Button} from 'santd';
+import 'santd/es/icon/style';
+import 'santd/es/button/style';
+import './index.less';
 
 export default class Welcome extends Component {
 
     static template = /* html */`
         <div class="welcome">
-           Welcome
+            <div class="logo-wrapper">
+                <img src="https://baidu.github.io/san/img/logo-colorful.svg" class="logo"/>
+            </div>
+            <div class="title">
+                {{text.welcome.content.title}}
+            </div>
+            <div class="tips">
+                <div s-for="n in num" class="tip">
+                    <s-icon type="{{tipIcons[n - 1]}}" style="font-size: 26px; color: #1890ff"/>
+                    <div class="message">
+                        {{text.welcome.content.tips[n - 1]}}
+                    </div>
+                </div>
+            </div>
+            <div class="actions flex-none">
+                <s-button
+                    icon="check"
+                    type="primary"
+                    size="large"
+                    on-click="remove"
+                >{{text.welcome.content.ok}}</s-button>
+            </div>
         </div>
     `;
-
+    static components = {
+        's-icon': Icon,
+        's-button': Button
+    };
     initData() {
         return {
+            num: [1, 2, 3],
+            tipIcons: [
+                'dashboard',
+                'arrow-left',
+                'home'
+            ],
+            widget: {},
+            text: {}
         };
     }
     attached() {
         // eslint-disable-next-line no-console
-        console.log('welcome attached');
+        console.log('welcome attached', this.data.get());
+    }
+    remove() {
+        const id = this.data.get('widget.id');
+        this.dispatch('Widget:remove', id);
     }
 }
