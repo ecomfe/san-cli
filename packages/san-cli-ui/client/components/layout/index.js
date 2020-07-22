4732a0aa08c97be21be1f05509ddd39afee05bad
liuhuyue <liuhuyue@baidu.com>
2020-07-22 21:15:57 +0800

    :sparkles: 二级页切换项目时保证数据同步

--- packages/san-cli-ui/client/components/layout/index.js
+++ packages/san-cli-ui/client/components/layout/index.js
@@ -124,5 +124,7 @@ export default class ComponentLayout extends Component {
         res.data && this.data.set('projectCurrent', res.data.projectOpen);
 
         this.getRecentProjectList();
+        
+        location.reload();
     }
 }
