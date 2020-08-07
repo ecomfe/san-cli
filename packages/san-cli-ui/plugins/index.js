6c60bc888b496fa00a71b68dc08e350c60d94a1c
liuhuyue <liuhuyue@baidu.com>
2020-08-07 11:46:40 +0800

    :sparkles: 加载 eslint 插件前先判断是否项目有 eslint 配置

--- packages/san-cli-ui/plugins/index.js
+++ packages/san-cli-ui/plugins/index.js
@@ -3,8 +3,18 @@
  * @author zttonly
  */
 
+const { cosmiconfig } = require('cosmiconfig');
+const explorer = cosmiconfig('eslint', {
+    stopDir: process.cwd()
+});
+
 module.exports = api => {
     require('./widgets')(api);
     require('./sanConfig')(api);
-    require('./eslintConfig')(api);
+    explorer.search().then(result => {
+        if (result === null) {
+            return;
+        }
+        require('./eslintConfig')(api);
+    });
 };
