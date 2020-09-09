6af9528a0949e5a11e08acda74770799b2283091
zttonly <zttonly@gmail.com>
2020-09-09 20:56:01 +0800

    :art: formate console.js

--- packages/san-cli-ui/server/connectors/console.js
+++ packages/san-cli-ui/server/connectors/console.js
@@ -1,48 +1,45 @@
+/**
+ * @file console connectors
+ * @author zttonly
+ */
+
 const shortId = require('shortid');
-const pubsub = require('../main/pubsub');
 const events = require('../utils/events');
 const {CONSOLE_LOG_ADDED} = require('../utils/channels');
+const getContext = require('../utils/context');
 
-/**
- * @typedef Log
- * @prop {string} id
- * @prop {string} date
- * @prop {LogType} type
- * @prop {string} message
- */
-let logs = [];
+class Console {
+    constructor() {
+        this.logs = [];
 
-/**
- * @param {Log} log
- * @param {any} pubsub
- */
-const add = (log, pubsub) => {
-    /** @type {Log} */
-    const item = {
-        id: shortId.generate(),
-        date: new Date().toISOString(),
-        ...log
-    };
-    logs.push(item);
-    pubsub.publish(CONSOLE_LOG_ADDED, {
-        consoleLogAdded: item
-    });
-    return item;
-};
+        this.add({type: 'info'}, getContext());
 
-const list = () => logs;
+        events.on('log', log => this.add(log, getContext()));
+    }
+    add(log, context) {
 
-const last = () => (logs.length ? logs[logs.length - 1] : null);
+        const item = {
+            id: shortId.generate(),
+            date: new Date().toISOString(),
+            ...log
+        };
+        this.logs.push(item);
+        context.pubsub.publish(CONSOLE_LOG_ADDED, {
+            consoleLogAdded: item
+        });
+        return item;
+    }
+    list() {
+        return this.logs;
+    }
+    last() {
+        let logs = this.logs;
+        return logs.length ? logs[logs.length - 1] : null;
+    }
+    clear() {
+        this.logs = [];
+        return this.logs;
+    }
+}
 
-const clear = () => logs = [];
-
-events.on('log', log => {
-    add(log, pubsub);
-});
-
-module.exports = {
-    add,
-    list,
-    last,
-    clear
-};
+module.exports = new Console();
