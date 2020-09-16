bd87d2960879d853a0f02fc867896c9ec834e5ae
liuhuyue <liuhuyue@baidu.com>
2020-09-16 22:50:04 +0800

    :lipstick: 界面优化：dependency 页面

--- packages/san-cli-ui/client/components/dependency/dependency-filter.js
+++ packages/san-cli-ui/client/components/dependency/dependency-filter.js
@@ -1,6 +1,6 @@
 /**
  * @file 已安装的package检索框
- * @author sunxiaoyu333
+ * @author sunxiaoyu333, Lohoyo
  */
 
 import Component from '@lib/san-component';
@@ -8,13 +8,12 @@ import './dependency-filter.less';
 
 export default class SearchBox extends Component {
     static template = /* html */`
-        <div class="dependency-filter">
-            <s-input-search
-                placeholder="{{$t('dependency.searchPlaceholder')}}"
-                value="{=filterKeyword=}"
-                size="large">
-            </s-input-search>
-        </div>
+        <s-input-search
+            placeholder="{{$t('dependency.searchPlaceholder')}}"
+            value="{=filterKeyword=}"
+            size="large"
+            class="dependency-filter">
+        </s-input-search>
     `;
 
     attached() {
