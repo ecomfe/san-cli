dfe065539eac61d738617c81fdb65017917bc068
liuhuyue <liuhuyue@baidu.com>
2020-09-21 17:23:44 +0800

    :lipstick: 把项目创建选项的滚动条挪到外边

--- packages/san-cli-ui/client/components/project/create.js
+++ packages/san-cli-ui/client/components/project/create.js
@@ -13,32 +13,34 @@ import './create.less';
 export default class ProjectCreate extends Component {
     static template = /* html */`
         <div class="flex-all project-options">
-            <s-form
-                label-col="{{formItemLayout.labelCol}}"
-                wrapper-col="{{formItemLayout.wrapperCol}}"
-                colon="{{false}}">
-                <s-formitem label="{{$t('project.components.create.folderName')}}">
-                    <s-input value="{=app.name=}"></s-input>
-                    <div class="grey">{{cwd}}/{{app.name}}</div>
-                </s-formitem>
-            </s-form>
+            <div class="container">
+                <s-form
+                    label-col="{{formItemLayout.labelCol}}"
+                    wrapper-col="{{formItemLayout.wrapperCol}}"
+                    colon="{{false}}">
+                    <s-formitem label="{{$t('project.components.create.folderName')}}">
+                        <s-input value="{=app.name=}"></s-input>
+                        <div class="grey">{{cwd}}/{{app.name}}</div>
+                    </s-formitem>
+                </s-form>
 
-            <c-prompts-form
-                s-ref="form"
-                prompts="{{prompts}}"
-                hide-submit-btn="true"
-                submit-text="{{$t('project.components.create.submitText')}}"
-                on-submit="onPromptsFormSubmit"
-                dropdownClassName="create-dropdown"
-                dropdownStyle="{{{'border-radius': '21px'}}}">
-            </c-prompts-form>
+                <c-prompts-form
+                    s-ref="form"
+                    prompts="{{prompts}}"
+                    hide-submit-btn="true"
+                    submit-text="{{$t('project.components.create.submitText')}}"
+                    on-submit="onPromptsFormSubmit"
+                    dropdownClassName="create-dropdown"
+                    dropdownStyle="{{{'border-radius': '21px'}}}">
+                </c-prompts-form>
 
-            <div s-if="isCreating" class="loading">
-                <s-spin tip="{{loadingTip}}" 
-                    spinning="{{isCreating}}"
-                    size="large">
-                    <s-icon slot="indicator" type="loading" style="font-size: 30px;" ></s-icon>
-                </s-spin>
+                <div s-if="isCreating" class="loading">
+                    <s-spin tip="{{loadingTip}}" 
+                        spinning="{{isCreating}}"
+                        size="large">
+                        <s-icon slot="indicator" type="loading" style="font-size: 30px;" ></s-icon>
+                    </s-spin>
+                </div>
             </div>
         </div>
     `;
