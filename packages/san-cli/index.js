a21a87bf00f2575bea1cc77ea8f91431a190b56a
ksky521 <ksky521@gmail.com>
2021-01-27 15:16:12 +0800

    :sparkles: cli精简&拆分

--- packages/san-cli/index.js
+++ packages/san-cli/index.js
@@ -13,8 +13,8 @@
 /* eslint-disable no-console */
 const updateNotifier = require('update-notifier');
 const semver = require('semver');
-const {error, chalk} = require('san-cli-utils/ttyLogger');
-const Commander = require('./lib/Commander');
+const chalk = require('chalk');
+
 const {
     scriptName,
     engines: {node: requiredNodeVersion},
@@ -22,21 +22,65 @@ const {
     version: pkgVersion
 } = require('./package.json');
 
-// set process
-process.title = scriptName;
-require('./lib/processLog');
-// 1. 检测 node 版本
+// 1. set process
+setProcess(scriptName);
+// 2. 检测 node 版本
 checkNodeVersion(requiredNodeVersion, pkgName);
-// 2. 检测最新版本
-upNotifier(pkgVersion, pkgName);
-// 3. 加载全部的命令
-const cli = new Commander();
-// 4. 触发执行
-cli.run(process.argv.slice(2));
+// 3. 执行命令
+execCommand();
+
+function execCommand(argv = process.argv) {
+    const cmdName = process.argv[2];
+    if (['-v', '-V', '--version'].includes(cmdName)) {
+        console.log(pkgVersion);
+        return;
+    }
+    if (['-h', '--help'].includes(cmdName)) {
+        console.log(`For more information, visit ${chalk.cyan('https://ecomfe.github.io/san-cli')}`);
+        return;
+    }
+    if (!cmdName || cmdName.startsWith('-')) {
+        console.error(
+            `No command is given, you can install any ${chalk.cyan('san-cli-*')} package to install a command`
+        );
+        return;
+    }
+    try {
+        // load command
+        const subCommands = [`san-cli-${cmdName}`, `@baidu/san-cli-${cmdName}`];
+        for (let i = 0, len = subCommands.length; i < len; i++) {
+            const subPkgName = subCommands[i];
+            const command = require(subPkgName);
+            if (command && command.command) {
+                const {name, version} = require(`${subPkgName}/package.json`);
+                console.log(`${pkgName}@${pkgVersion}/${name}@${version}`);
+                // 检查子模块更新情况
+                upNotifier(version, name);
+                require('yargs')
+                    .scriptName(scriptName)
+                    .usage('$0 <cmd> [args]')
+                    .command(command)
+                    .help()
+                    .alias('help', 'h')
+                    .alias('version', 'v').argv;
+
+                break;
+            }
+        }
+    } catch (e) {
+        if (e.code === 'MODULE_NOT_FOUND') {
+            // 没找到
+            console.error(chalk.red(`${cmdName} command not found, you may install san-cli-${cmdName}`));
+        } else {
+            console.error(e);
+        }
+        process.exit(1);
+    }
+}
 
 function checkNodeVersion(wanted, id) {
     if (!semver.satisfies(process.version, wanted)) {
-        error(
+        console.error(
             // prettier-ignore
             // eslint-disable-next-line
             'You are using Node ' + process.version + ', but this version of ' + id +
@@ -67,5 +111,18 @@ function upNotifier(version, name) {
             notifier && notifier.notify();
             process.exit(0);
         });
+    });
+}
+
+function setProcess(scriptName) {
+    process.title = scriptName;
+    process.on('uncaughtException', error => {
+        console.error(error);
+        process.exit(1);
+    });
+
+    process.on('unhandledRejection', error => {
+        console.error(error);
+        process.exit(1);
     });
 }
