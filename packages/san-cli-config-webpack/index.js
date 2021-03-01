425fcd32c007871105b1996366f7549a7cdc1ac1
jinzhan <jinzhan@baidu.com>
2021-03-01 17:23:35 +0800

    :sparkles: 增加设置plugin/rules/alias/minimizers的快捷方法

--- packages/san-cli-config-webpack/index.js
+++ packages/san-cli-config-webpack/index.js
@@ -5,6 +5,44 @@ const Config = require('webpack-chain');
 
 const {normalizeProjectOptions} = require('./utils');
 const {devServerOptions} = require('./defaultOptions');
+
+const getChainMap = (config, methods) => {
+    const m = methods.split('.');
+    let root = config[m[0]];
+    let subMethod;
+    while ((subMethod = m.pop())) {
+        if (root[subMethod]) {
+            root = root[subMethod];
+        }
+    }
+    return root;
+};
+
+[
+    ['Plugin', 'plugins'],
+    ['Rule', 'module.rules'],
+    ['Alias', 'resolve.alias'],
+    ['Minimizer', 'optimization.minimizers']
+].forEach(([key, methods]) => {
+    /**
+     * @example
+     * const {delAlias} = require('./');
+     * delAlias('@').setAlias('@', '...')
+     */
+    exports[`del${key}`] = (config, name) => {
+        getChainMap(config, methods).delete(name);
+        return exports;
+    };
+    exports[`get${key}`] = (config, name) => {
+        getChainMap(config, methods).get(name);
+        return exports;
+    };
+    exports[`set${key}`] = (config, name, value) => {
+        getChainMap(config, methods).set(name, value);
+        return exports;
+    };
+});
+
 exports.createChainConfig = function getChainConfig(mode, projectOptions, webpackConfig = new Config()) {
     ['base', 'pages', 'style', mode].forEach(name =>
         require(`./configs/${name}`)(webpackConfig, projectOptions ? normalizeProjectOptions(projectOptions) : {})
